eb085caf853519ef17b22c0fab391b10dd044360
Shunta Karasawa <krswshnt@gmail.com>
2016-08-31 10:37:23 +0900

    Get data from db

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-var fs = require('fs');
-var http = require('http');
-var server = http.createServer();
+var express = require('express');
+var app = express();
 
-server.on('request', function(req, res) {
-	var stream = fs.createReadStream('index.html');
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	stream.pipe(res);
+var fs = require('fs');
+var ejs = require('ejs');
+var template = fs.readFileSync(__dirname + '/views/pages/index.ejs', 'utf-8');
+
+var mysql = require('mysql');
+var db_cfg = require('./config/db_settings.js');
+var connection = mysql.createConnection({
+	host    : db_cfg.host,
+	port    : db_cfg.port,
+	user    : db_cfg.user,
+	password: db_cfg.passwd,
+	database: db_cfg.name
 });
-server.listen(8080);
+
+var d;
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+});
+
+connection.query('SELECT value from data', function(error, results, fields) {
+	d = results[0].value;
+});
+app.get('/', function(req, res) {
+	var data = ejs.render(template,{ d: d});
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(data);
+	res.end();
+});
+
+app.listen(8080);
+console.log('server running...');
