0f286257ebad3f466ae033fe624ecc524cdda90c
Shunta Karasawa <krswshnt@gmail.com>
2016-08-31 14:04:46 +0900

    Change templete engine, ejs2pug

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 var fs = require('fs');
-var ejs = require('ejs');
-var template = fs.readFileSync(__dirname + '/views/pages/index.ejs', 'utf-8');
+//var ejs = require('ejs');
+//var template = fs.readFileSync(__dirname + '/views/pages/index.ejs', 'utf-8');
 
 var mysql = require('mysql');
 var db_cfg = require('./config/db_settings.js');
@@ -26,11 +26,14 @@ connection.connect(function (err) {
 connection.query('SELECT value from data', function(error, results, fields) {
 	d = results[0].value;
 });
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
-	var data = ejs.render(template,{ d: d});
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write(data);
-	res.end();
+	//var data = ejs.render(template,{ d: d});
+	//res.writeHead(200, {'Content-Type': 'text/html'});
+	res.render('index', {eventTitle : 'イベントの名前', level: 1});
 });
 
 app.listen(8080);
